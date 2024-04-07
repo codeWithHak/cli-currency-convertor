15f512706eb56ff3d0e077ef8eaa9ccf3d364284
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-04-07 08:05:30 +0500

    add fixed conversion for five currencies

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-"use strict";
-let myInfo = {
-    name: "huzair",
-    age: 18,
-    email: "h@h.com"
+import inquirer from "inquirer";
+let currencies = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
 };
-let property = "age";
-console.log(myInfo[property]);
+let user_answer = await inquirer.prompt([{
+        name: "from",
+        message: "enter from message",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "to",
+        message: "enter to message",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "set amount",
+        type: "number"
+    }
+]);
+let fromAmount = currencies[user_answer.from];
+let toAmount = currencies[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convert = baseAmount * toAmount;
+console.log(fromAmount);
+console.log(toAmount);
+console.log(amount);
+console.log(baseAmount);
+console.log(convert.toFixed(2));
